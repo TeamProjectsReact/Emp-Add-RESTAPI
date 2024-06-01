acb09eb42792df958312a0478fb1ee2bf66969fc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-01 08:28:51 +0530

    Update authController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -15,6 +15,30 @@ const AuthController = {
             if(checkUser) {
                 return res.json({Error: "User Already in Database"})
             }
+            else{
+                // hash password
+                const hashPass = bcrypt.hash(password, 10)
+
+                // insert user
+                const UserNew = new User({
+                    username,
+                    email,
+                    password: hashPass,
+                    Role: 'Users'
+                })
+
+                // save user
+                const ResultUser = UserNew.save()
+
+                if(ResultUser) {
+                    return res.json({Status: "Success"})
+                }
+                else{
+                    return res.json({Error: "Internal Server Error"})
+                }
+            }
+
+
         }
         catch (err) {
             return res.json({Error: "Internal Server ERROR"})
