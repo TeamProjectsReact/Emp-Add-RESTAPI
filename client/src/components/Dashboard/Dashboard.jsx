8eda31e641753407d15ad7cbe804a2a58c76d61b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-01 11:24:38 +0530

    Create Dashboard.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import secureLocalStorage from 'react-secure-storage';
+
+const Dashboard = () => {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
