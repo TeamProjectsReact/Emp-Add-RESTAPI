de62b7320a3ec44dd47b1f12a99d0858adda33f9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-02 07:44:30 +0530

    Update Dashboard.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 import secureLocalStorage from 'react-secure-storage';
 
 const Dashboard = () => {
+  const navigate = useNavigate()
+  const EmailUser = secureLocalStorage.getItem("Login1");
+  const RoleUser = secureLocalStorage.getItem("Login2");
   return (
     <div>Dashboard</div>
   )
