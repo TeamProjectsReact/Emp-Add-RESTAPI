b5dd1bfd985719e73671e3078aa8a6cac47d0fc4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-02 08:15:36 +0530

    Update EmployeeController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/EmployeeController.js
+++ server/Controllers/EmployeeController.js
@@ -23,6 +23,28 @@ const EmployeeController = {
 
         // check employee already in database
         const checkEmp =  await Employee.findOne({ empID, empEmail, NIC })
+
+        if(checkEmp){
+            return res.json({ Error: "Employee already in Database"})
+        }
+        else{
+            const NewEmp = new Employee({
+                empID, 
+                empEmail, 
+                Initials, 
+                Surname, 
+                Phone, 
+                NIC, 
+                Address, 
+                Category, 
+                dob, 
+                emgContact, 
+                Type, 
+                CivilState, 
+                Gender, 
+                Religion
+            })
+        }
     }
 }
 
