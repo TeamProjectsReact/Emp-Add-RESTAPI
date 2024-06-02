084f9644344e882fec96328d42b0e8796e3fd534
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-02 07:27:24 +0530

    Update AddEmployee.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Employee/AddEmployee.jsx
+++ client/src/components/Employee/AddEmployee.jsx
@@ -91,6 +91,15 @@ const AddEmployee = () => {
                                         <option value="Temporary">Temporary</option>     
                                     </select>
                                 </div>
+                                <div className="my-2">
+                                    <label htmlFor="" className="text-gray-500">Civil Status</label>
+                                    <select className="h-12 w-full rounded bg-gray-200 pl-2 my-2 shadow-md" required
+                                    >
+                                        <option>Select Option</option>
+                                        <option value="Married">Married</option>
+                                        <option value="Single">Single</option>   
+                                    </select>
+                                </div>
                             </div>
 
 
