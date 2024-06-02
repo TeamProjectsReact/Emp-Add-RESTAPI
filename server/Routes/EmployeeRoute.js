96a7824a1c666a26b24b1f23d2cbc4b7a10f944f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-02 08:19:00 +0530

    Update EmployeeRoute.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Routes/EmployeeRoute.js
+++ server/Routes/EmployeeRoute.js
@@ -3,3 +3,6 @@ const EmployeeController = require('../Controllers/EmployeeController')
 
 const router = express.Router()
 
+router.post('/AddEmp', EmployeeController.AddEmp)
+
+module.exports = router;
