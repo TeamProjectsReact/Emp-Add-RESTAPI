885af7a353319e2fa203f4d1f0279fad9eab649a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-02 07:29:42 +0530

    Update AddEmployee.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Employee/AddEmployee.jsx
+++ client/src/components/Employee/AddEmployee.jsx
@@ -110,6 +110,10 @@ const AddEmployee = () => {
                                         <option value="Other">Other</option>
                                     </select>
                                 </div>
+                                <div className="my-2">
+                                    <label htmlFor="" className="text-gray-500">Religion : </label>
+                                    <input type="text" name="" id="" className="h-12 w-full rounded bg-gray-200 pl-2 my-2 shadow-md" required placeholder='Enter Employee Religion' />
+                                </div>
                             </div>
 
 
