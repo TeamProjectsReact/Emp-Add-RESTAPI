d1359c502c9b894d9795514eb22a0571a967dda4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-02 06:53:24 +0530

    Update AddEmployee.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Employee/AddEmployee.jsx
+++ client/src/components/Employee/AddEmployee.jsx
@@ -1,10 +1,11 @@
-import React, {useEffect, useNavigate} from 'react'
+import React, {useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 import secureLocalStorage from 'react-secure-storage';
 
 const AddEmployee = () => {
-    const navigate = useNavigate();
-    const RoleUser = secureLocalStorage.getItem("Login1");
-    const EmailUser = secureLocalStorage.getItem("login2");
+    const navigate = useNavigate()
+    const EmailUser = secureLocalStorage.getItem("Login1");
+    const RoleUser = secureLocalStorage.getItem("Login2");
 
     if(RoleUser !== null && EmailUser !== null && RoleUser === "SuperAdmin"){
         return (
